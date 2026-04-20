bdd34718cf3347248c8a40695ccba308c5bcfeae
Sean Murphy <sean@iamseanmurphy.com>
2026-04-20 09:00:19 +0000

    6.5.0.5

--- wpdatatables/templates/admin/dashboard/dashboard.inc.php
+++ wpdatatables/templates/admin/dashboard/dashboard.inc.php
@@ -338,12 +338,12 @@ $tableChartsCount = (int)WDTTools::getTablesCount('chart');
                                  </span>
                         </p>
                         <p class="wpdt-text wpdt-font m-b-18">
-                            New update:
+                            Minor security update:
                         </p>
                         <div class="alert alert-info m-b-0" role="alert">
                             <i class="wpdt-icon-info-circle-full"></i>
                             <ul>
-                                <li>Minor CSS fixes.</li>
+                                <li>Fixed issue with stored cross-site scripting via CSV/Excel data import.</li>
                                 <li>Other small bug fixes and stability improvements.</li>
                             </ul>
                         </div>
