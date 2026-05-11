fc4e7702a2d17e25712fc917aff1fafe6ccefd31
Sean Murphy <sean@iamseanmurphy.com>
2026-05-11 07:32:18 +0000

    6.5.0.7

--- wpdatatables/readme.txt
+++ wpdatatables/readme.txt
@@ -6,7 +6,7 @@ Tags: table, table builder, datatable, csv, chart
 Requires at least: 4.0
 Tested up to: 6.9.4
 Requires PHP: 7.4
-Stable tag: 6.5.0.6
+Stable tag: 6.5.0.7
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -442,6 +442,10 @@ Please report security bugs found in the source code of the wpDataTables plugin 
 13. Google chart settings
 
 == Changelog ==
+= 6.5.0.7 =
+* Added support for HTML, Rating, and Signature fileds for the Ivyforms integration.
+Other small bug fixes and stability improvements.
+
 = 6.5.0.6 =
 * Minor CSS adjustments.
 Other small bug fixes and stability improvements.
--- wpdatatables/templates/admin/dashboard/dashboard.inc.php
+++ wpdatatables/templates/admin/dashboard/dashboard.inc.php
@@ -338,12 +338,12 @@ $tableChartsCount = (int)WDTTools::getTablesCount('chart');
                                  </span>
                         </p>
                         <p class="wpdt-text wpdt-font m-b-18">
-                            Minor security update:
+                            New update:
                         </p>
                         <div class="alert alert-info m-b-0" role="alert">
                             <i class="wpdt-icon-info-circle-full"></i>
                             <ul>
-                                <li>Minor CSS adjustments.</li>
+                                <li>Added support for HTML, Rating, and Signature fileds for the Ivyforms integration.</li>
                                 <li>Other small bug fixes and stability improvements.</li>
                             </ul>
                         </div>
